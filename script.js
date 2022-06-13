4b2bc705edfa2fcb7d5880b35097852a4b744389
miraybuy <miraybuyukkaray@gmail.com>
2022-06-13 20:49:45 +0200

    update

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function computation() {
   } else if (calculation === "/") {
     if (parseFloat(currentNum) == "0") {
       alert("infinity");
-      currentDisplay.innerText.slice(0, -1);
+      result = " ";
     } else {
       result = parseFloat(result) / parseFloat(currentNum);
     }

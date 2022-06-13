8879a3e331c0810727bf88ac5b3b06e92c2c19cb
miraybuy <miraybuyukkaray@gmail.com>
2022-06-13 16:36:51 +0200

    built the calculator

--- script.js
+++ script.js
@@ -1,17 +1,96 @@
 const previousDisplay = document.querySelector(".previousDisplay");
 const currentDisplay = document.querySelector(".currentDisplay");
+const cornerDisplay = document.querySelector(".cornerDisplay");
 const numbers = document.querySelectorAll(".btn-number");
 const operations = document.querySelectorAll(".btn-operation");
 const deleteLast = document.querySelector(".btn-delete");
-const clear = document.querySelector(".btn-all-clear");
+const clear = document.querySelector(".btn-clear");
 const equals = document.querySelector(".btn-equals");
 
 let prevNum = "";
 let currentNum = "";
+let appendDot = false;
+let result = null;
+let calculation = "";
 
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !appendDot) {
+      appendDot = true;
+    } else if (e.target.innerText === "." && appendDot) {
+      return;
+    }
     currentNum += e.target.innerText;
     currentDisplay.innerText = currentNum;
   });
+});
+
+operations.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    if (!currentNum) return;
+    appendDot = false;
+    const operationType = e.target.innerText;
+    if (prevNum && currentNum && calculation) {
+      computation();
+    } else {
+      result = parseFloat(currentNum);
+    }
+    clearDigit(operationType);
+    calculation = operationType;
+  });
+});
+
+function clearDigit(type = "") {
+  prevNum += currentNum + " " + type + " ";
+  previousDisplay.innerHTML = prevNum;
+  currentDisplay.innerHTML = "";
+  currentNum = "";
+  previousDisplay.innerHTML = result;
+  cornerDisplay.innerText = result;
+}
+
+function computation() {
+  if (calculation === "+") {
+    result = parseFloat(result) + parseFloat(currentNum);
+  } else if (calculation === "-") {
+    result = parseFloat(result) - parseFloat(currentNum);
+  } else if (calculation === "X") {
+    if (parseFloat(result) === "0" || parseFloat(currentNum) === "0") {
+      result = "0";
+    } else {
+      result = parseFloat(result) * parseFloat(currentNum);
+    }
+  } else if (calculation === "/") {
+    if (parseFloat(currentNum) == "0") {
+      alert("error");
+      result = "0";
+    } else {
+      result = parseFloat(result) / parseFloat(currentNum);
+    }
+  }
+}
+
+equals.addEventListener("click", () => {
+  if (!prevNum || !currentNum) return;
+  appendDot = false;
+  computation();
+  clearDigit();
+  currentDisplay.innerText = result;
+  cornerDisplay.innerText = "";
+  currentNum = result;
+  prevNum = "";
+});
+
+clear.addEventListener("click", () => {
+  prevNum = "";
+  currentNum = "";
+  prevDisplay.innerText = "";
+  currentDisplay.innerText = "";
+  result = "";
+  cornerDisplay.innerText = "";
+});
+
+deleteLast.addEventListener("click", () => {
+  currentDisplay.innerText = currentDisplay.innerText.slice(0, -1);
+  currentNum = currentDisplay.innerText;
 });
